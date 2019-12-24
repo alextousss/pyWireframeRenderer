67f39ee1456cd39d3bdfcee01b886da6f6ca4d8d
alex <toussaintalex@free.fr>
2019-12-24 13:47:55 +0100

    file argument from command line + optimizations

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ import pywavefront
 import numpy as np
 import math
 import time
+import sys
+
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 1000
-SPEED=1
+SPEED = 1
+
+Vl = -WINDOW_HEIGHT/2
+Vb = WINDOW_HEIGHT/2
+Vt = -WINDOW_WIDTH/2
+Vr = WINDOW_WIDTH/2
+
+
+def NDCToScreen(matrix):
+    return np.matmul(
+        np.array([
+            [(Vr-Vl)/2, 0, 0, (Vr+Vl)/2],
+            [0, (Vt-Vb)/2, 0, (Vt+Vb)/2],
+            [0, 0, 1/2, 1/2],
+            [0, 0, 0, 1],
+        ]),
+        matrix
+    )
+
 
 def translate(matrix, x, y, z):
     return np.matmul(
@@ -31,48 +51,42 @@ def scale(matrix, number):
     )
 
 
-def rotate(matrix, angle):
+def rotate(matrix, phi, psi, theta):
+    cos = math.cos
+    sin = math.sin
     return np.matmul(
             np.array([
-                [1, 0, 0, 0],
-                [0, math.cos(angle), -math.sin(angle), 0],
-                [0, math.sin(angle), math.cos(angle), 0],
+                [cos(theta)*cos(psi), -cos(phi)*sin(psi)+sin(phi)*sin(theta)*cos(psi), sin(phi)*sin(psi)+cos(phi)*sin(theta)*cos(psi), 0],
+                [cos(theta)*sin(psi), cos(phi)*cos(psi)+sin(phi)*sin(theta)*sin(psi), -sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi), 0],
+                [-sin(theta), sin(psi)*cos(theta), cos(psi)*sin(theta), 0],
                 [0, 0, 0, 1]
             ]),
             matrix
     )
 
 
-def project(matrix, c):
-    interm = np.matmul(
-            np.array([
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, -1/c, 1]
-            ]),
-            matrix
+def perspective(matrix, c):
+    return np.matmul(
+        np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, -1/c, 1]
+        ]),
+        matrix
     )
-    for i in range(3):
-        for j in range(len(interm[0])):
-            interm[i][j] /= interm[3][j]
-    return interm
 
+
+def project(matrix):
+    for i in range(3):
+        matrix[i] = np.divide(matrix[i], matrix[3])
+    return matrix
 
 
 def getScreenCoordsFromNDC(coords):
     return (
         (coords[0] + 1)/2 * WINDOW_WIDTH,
-        WINDOW_HEIGHT - ( (coords[1] + 1)/2 * WINDOW_HEIGHT )
-    )
-
-
-def drawNDCLine(screen, color, fromCoords, toCoords):
-    pygame.draw.line(
-        screen, 
-        color, 
-        getScreenCoordsFromNDC(fromCoords), 
-        getScreenCoordsFromNDC(toCoords), 
+        WINDOW_HEIGHT - ((coords[1] + 1)/2 * WINDOW_HEIGHT)
     )
 
 
@@ -95,17 +109,17 @@ def getFacesAndVerticesFromOBJ(filename):
     return handler.mesh_list[0].faces, handler.vertices
 
 
-
 def run():
     start_time = time.time()
+    pygame.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     running = 1
     
-    faces, vectors = getFacesAndVerticesFromOBJ("cube.obj") 
+    faces, vectors = getFacesAndVerticesFromOBJ(sys.argv[1]) 
     print(faces)
     print(vectors)
-    # vectors = getVectorsFromOBJ("cube.obj")
     x, y, z = (0, 0, 3)
+    psi, phi, theta = (0, 0, 0)
     npVectors = [
         [],
         [],
@@ -119,25 +133,32 @@ def run():
         npVectors[1].append(vector[1])
         npVectors[2].append(vector[2])
         npVectors[3].append(1)
-    
+
+    font = pygame.font.SysFont("mono", 20)
+
     r = 0
     while running:
-        disVectors = scale(
+        disVectors = NDCToScreen( 
+            scale(
                 project(
-                    translate(
-                        rotate(
-                            npVectors, 
-                            r,
+                    perspective(
+                        translate(
+                            rotate(
+                                npVectors, 
+                                psi,
+                                phi,
+                                theta
+                            ), 
+                            x,
+                            y,
+                            z,
                         ), 
-                        x,
-                        y,
-                        z,
-                    ), 
-                    10
+                        10
+                    )
                 ),
-                0.2
+                0.2,
             )
-
+        )
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             running = 0
@@ -150,20 +171,26 @@ def run():
                             [disVectors[0][i], disVectors[1][i]], 
                             [disVectors[0][j], disVectors[1][j]])
         """
+
+        disVectors[0] += WINDOW_WIDTH/2
+        disVectors[1] += WINDOW_HEIGHT/2
         for face in faces:
-            drawNDCLine(screen, (255, 0, 0), 
-                        [disVectors[0][face[0]], disVectors[1][face[0]]], 
-                        [disVectors[0][face[1]], disVectors[1][face[1]]])
-            drawNDCLine(screen, (0, 255, 0), 
-                        [disVectors[0][face[1]], disVectors[1][face[1]]], 
-                        [disVectors[0][face[2]], disVectors[1][face[2]]])
-            drawNDCLine(screen, (0, 0, 255), 
-                        [disVectors[0][face[2]], disVectors[1][face[2]]], 
-                        [disVectors[0][face[0]], disVectors[1][face[0]]])
-        pygame.display.flip()
+            pygame.draw.lines(
+                screen, 
+                (255, 0, 0), 
+                True,
+                [
+                    (disVectors[0][face[0]], disVectors[1][face[0]]), 
+                    (disVectors[0][face[1]], disVectors[1][face[1]]),
+                    (disVectors[0][face[2]], disVectors[1][face[2]]),
+                ],
+            )
 
         pygame.event.pump()  # Allow pygame to handle internal actions.
         dt = time.time() - start_time
+        freq = 1/dt
+        text = font.render(str(int(freq)) + " FPS", True, (255, 255, 255))
+        screen.blit(text, (0, 0))
         start_time = time.time()
         key = pygame.key.get_pressed()
         if key[pygame.K_z]:
@@ -178,6 +205,27 @@ def run():
             z -= SPEED * dt
         if key[pygame.K_e]:
             z += SPEED * dt
-        if key[pygame.K_r]:
-            r += SPEED * dt
-run()
+        
+        if key[pygame.K_i]:
+            psi += SPEED * dt
+        if key[pygame.K_k]:
+            psi -= SPEED * dt
+        if key[pygame.K_j]:
+            theta += SPEED * dt
+        if key[pygame.K_l]:
+            theta -= SPEED * dt
+        if key[pygame.K_u]:
+            phi += SPEED * dt
+        if key[pygame.K_o]:
+            phi -= SPEED * dt
+        
+        pygame.display.flip()
+import cProfile, pstats, io
+pr = cProfile.Profile()
+pr.enable()
+try:
+    run()
+except:
+    pass
+pr.disable()
+pr.dump_stats("prof.prof")
